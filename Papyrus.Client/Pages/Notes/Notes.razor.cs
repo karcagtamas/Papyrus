bf7b929e2778a76f22ffd983863ea4d7cbd96087
Karcag Tamas <karcagtamas@outlook.com>
2022-08-19 21:17:03 +0200

    #266 Add user note page

---
+++ Papyrus.Client/Pages/Notes/Notes.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Papyrus.Client.Services.Notes.Interfaces;
+using Papyrus.Shared.DTOs.Notes;
+using Papyrus.Shared.Enums.Notes;
+
+namespace Papyrus.Client.Pages.Notes;
+
+public partial class Notes : ComponentBase
+{
+    [Parameter]
+    public int GroupId { get; set; }
+
+    [Inject]
+    private INoteService NoteService { get; set; } = default!;
+
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+    private List<NoteLightDTO> NoteList { get; set; } = new();
+    private NoteSearchType SearchType { get; set; } = NoteSearchType.All;
+
+    protected override async void OnInitialized()
+    {
+        await Refresh();
+        base.OnInitialized();
+    }
+
+    private async Task Refresh()
+    {
+        NoteList = await NoteService.GetByUser(SearchType);
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task Create()
+    {
+        var result = await NoteService.CreateEmpty(null);
+
+        if (ObjectHelper.IsNotNull(result))
+        {
+            await JSRuntime.InvokeAsync<object>("open", $"/notes/editor/{result.Id}", "_blank");
+        }
+    }
+
+    private async Task HandleSearchTypeChange(NoteSearchType searchType)
+    {
+        SearchType = searchType;
+        await Refresh();
+    }
+}
