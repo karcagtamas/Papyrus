bbe758604cf835eb52693460bd4615cb4cbf7035
Karcag Tamas <karcagtamas@outlook.com>
2022-08-28 19:53:54 +0200

    #81 About and Home localization with markup display

---
+++ Papyrus.Client/Utils/MarkupConverter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Papyrus.Client.Utils;
+
+public static class MarkupConverter
+{
+    public static MarkupString AsMarkup(this LocalizedString value) => new(value);
+}
