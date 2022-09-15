43e5727d0cec57d31b3c0ad5b245b905a29b92af
Karcag Tamas <karcagtamas@outlook.com>
2022-09-15 20:34:13 +0200

    #311 Fix missing multiple action logs

--- Papyrus.Logic/Services/Interfaces/IActionLogService.cs
+++ Papyrus.Logic/Services/Interfaces/IActionLogService.cs
@@ -5,5 +5,5 @@ namespace Papyrus.Logic.Services.Interfaces;
 
 public interface IActionLogService : IMapperRepository<ActionLog, long>
 {
-    void AddActionLog(string key, string segment, string type, string? performer, Dictionary<string, string> texts);
+    void AddActionLog(string key, string segment, string type, string? performer, Dictionary<string, string> texts, bool doPersist = false);
 }
--- Papyrus.Logic/Services/Notes/Interfaces/INoteActionLogService.cs
+++ Papyrus.Logic/Services/Notes/Interfaces/INoteActionLogService.cs
@@ -5,6 +5,6 @@ namespace Papyrus.Logic.Services.Notes.Interfaces;
 
 public interface INoteActionLogService
 {
-    void AddActionLog(string note, string performer, NoteActionLogType type);
+    void AddActionLog(string note, string performer, NoteActionLogType type, bool doPersist = false);
     IQueryable<ActionLog> GetQuery(string noteId);
 }
