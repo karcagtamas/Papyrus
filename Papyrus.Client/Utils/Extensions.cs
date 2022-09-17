529d216435a553b3a26c8d027005d900d38318a2
Karcag Tamas <karcagtamas@outlook.com>
2022-09-17 20:10:55 +0200

    #296 Add Max Throttle timeout

--- Papyrus.Client/Utils/Extensions.cs
+++ Papyrus.Client/Utils/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using KarcagS.Blazor.Common.Http;
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using KarcagS.Blazor.Common.Http;
 using Papyrus.Shared.Models.Notes;
 
 namespace Papyrus.Client.Utils;
@@ -13,5 +16,42 @@ public static class Extensions
             .Add("textFilter", query.TextFilter)
             .Add("dateFilter", query.DateFilter)
             .AddMultiple("tags", query.Tags);
+    }
+
+    public static IObservable<T> ThrottleMax<T>(this IObservable<T> source, TimeSpan dueTime, TimeSpan maxTime) => source.ThrottleMax(dueTime, maxTime, Scheduler.Default);
+
+    public static IObservable<T> ThrottleMax<T>(this IObservable<T> source, TimeSpan dueTime, TimeSpan maxTime, IScheduler scheduler)
+    {
+        return Observable.Create<T>(o =>
+        {
+            var hasValue = false;
+            T value = default(T)!;
+
+            var maxTimeDisposable = new SerialDisposable();
+            var dueTimeDisposable = new SerialDisposable();
+
+            Action action = () =>
+            {
+                if (hasValue)
+                {
+                    maxTimeDisposable.Disposable = Disposable.Empty;
+                    dueTimeDisposable.Disposable = Disposable.Empty;
+                    o.OnNext(value);
+                    hasValue = false;
+                }
+            };
+
+            return source.Subscribe(x =>
+            {
+                if (!hasValue)
+                {
+                    maxTimeDisposable.Disposable = scheduler.Schedule(maxTime, action);
+                }
+
+                hasValue = true;
+                value = x;
+                dueTimeDisposable.Disposable = scheduler.Schedule(dueTime, action);
+            }, o.OnError, o.OnCompleted);
+        });
     }
 }
