ac51302d3fdc01d72cc6482275dca9140f5dcc75
Karcag Tamas <karcagtamas@outlook.com>
2022-09-22 18:09:43 +0200

    #107 Theme storing on client side

--- Papyrus.Client/Services/CommonService.cs
+++ Papyrus.Client/Services/CommonService.cs
@@ -1,5 +1,6 @@
 using KarcagS.Blazor.Common.Http;
 using KarcagS.Blazor.Common.Models;
+using KarcagS.Blazor.Common.Store;
 using Papyrus.Client.Services.Interfaces;
 using Papyrus.Shared.DTOs;
 
@@ -8,10 +9,12 @@ namespace Papyrus.Client.Services;
 public class CommonService : ICommonService
 {
     private readonly IHttpService httpService;
+    private readonly IStoreService storeService;
     private readonly string url = $"{ApplicationSettings.BaseApiUrl}/Editor";
 
-    public CommonService(IHttpService httpService)
+    public CommonService(IHttpService httpService, IStoreService storeService)
     {
+        this.storeService = storeService;
         this.httpService = httpService;
     }
 
@@ -33,6 +36,12 @@ public class CommonService : ICommonService
         var settings = new HttpSettings(httpService.BuildUrl(url, "Theme", "User"));
 
         return httpService.GetInt(settings).ExecuteWithResultOrElse(0);
+    }
+
+    public async Task SetLocalTheme(int key)
+    {
+        await SetUserTheme(key);
+        storeService.Add("theme", key);
     }
 
     public Task SetUserTheme(int key)
