85b423a8c6cee03a7affbc59bbb3d9c34e1b0bbc
Karcag Tamas <karcagtamas@outlook.com>
2022-09-22 22:09:05 +0200

    #106 Fix image color and sidebar color

--- Papyrus.Client/Shared/PapyrusTheme.cs
+++ Papyrus.Client/Shared/PapyrusTheme.cs
@@ -1,5 +1,6 @@
 using KarcagS.Blazor.Common.Theme;
 using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace Papyrus.Client.Shared;
 
@@ -8,25 +9,27 @@ public static class PapyrusTheme
     public static readonly AppTheme AppTheme = new(
         new AppTheme.AppColorPalette
         {
-            MainColor = new("#0D2971"),
-            SecondaryColor = new("#581043"),
-            TertiaryColor = new("#361B3A"),
-            WarningColor = new("#C07C38"),
-            ErrorColor = new("#850525"),
-            InfoColor = new("#66ADA1"),
-            SuccessColor = new("#40921A"),
-            White = new("#FFFFFF"),
+            MainColor = "#0D2971",
+            SecondaryColor = "#581043",
+            TertiaryColor = "#361B3A",
+            WarningColor = "#C07C38",
+            ErrorColor = "#850525",
+            InfoColor = "#66ADA1",
+            SuccessColor = "#40921A",
+            White = "#FFFFFF",
+            DrawerBackground = new MudColor("#0D2971").ColorLighten(0.71)
         },
         new AppTheme.AppColorPalette
         {
-            MainColor = new("#537deb"),
-            SecondaryColor = new("#e053b7"),
-            TertiaryColor = new("#b16ebb"),
-            WarningColor = new("#e1bd9a"),
-            ErrorColor = new("#f84c77"),
-            InfoColor = new("#b2d6d0"),
-            SuccessColor = new("#95e570"),
-            White = new("#FFFFFF"),
+            MainColor = "#537deb",
+            SecondaryColor = "#e053b7",
+            TertiaryColor = "#b16ebb",
+            WarningColor = "#e1bd9a",
+            ErrorColor = "#f84c77",
+            InfoColor = "#b2d6d0",
+            SuccessColor = "#95e570",
+            White = "#FFFFFF",
+            DrawerBackground = new MudColor("#32333d").ColorDarken(0.1)
         });
 
     public static MudTheme GetTheme() => AppTheme.Theme;
